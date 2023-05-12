11f285f175675b1b70f75b2a3ce6a0b133e75b63
Disciplined-22 <129745308+Disciplined-22@users.noreply.github.com>
2023-05-12 16:53:42 +0530

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// import '../styles/modern-normalize.css';
+// import '../styles/style.css';
+
+// import '../styles/utils.css';
+
+// import mobileNav from './utils/mobile-nav';
+// import darkMode from './utils/dark-mode';
+import lazyLoading from './utils/lazy-loading';
+
+
+// mobileNav();
+// darkMode();
+lazyLoading();
+
